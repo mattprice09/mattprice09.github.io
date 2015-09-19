d995a97b15374e5cee2f007697c55b5d19f478cf
mattprice09 <mattprice09@rocketmail.com>
2015-09-19 04:07:00 -0500

    updates

--- tester.js
+++ tester.js
@@ -8,7 +8,7 @@ function on_request_success(response) {
 function on_request_error(r, text_status, error_thrown) {
     console.log(r);
     console.debug('error' + text_status + ", " + error_thrown + ":\n" + r.responseText);
-    document.write("Failure line 8");
+    document.write("Failure line 11");
 }
 
 var request = {"Data":"Success!!"};
@@ -16,7 +16,7 @@ var request = {"Data":"Success!!"};
 function addTrack() {
     $.ajax({
     url: 'http://mattprice09.github.io/addTrack.py',
-    type: 'POST',
+    type: ['GET', 'POST'],
     cache: false,
     data: JSON.stringify(request),
     contentType: 'application/json',
