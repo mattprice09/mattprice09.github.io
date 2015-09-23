3aa3ab3204f87fae1d291ab7dc2fd6ee8f0a3650
mattprice09 <mattprice09@rocketmail.com>
2015-09-23 12:03:08 -0500

    transferred files from WUHack to IO

--- spotifyAPI.js
+++ spotifyAPI.js
@@ -3,8 +3,31 @@ var clientSecret = "d19a4e03cd31497981aa0ee5aa891eeb";
 var spotifyBase = "https://api.spotify.com/";
 
 
-var bearer = "BQDuYYT0PenIDZBucwETH9uksuUzd4S9Twl7WR8Zsch5zKX9r141tR-hNH33wIYcC-LkYUgz4TFuz__cpUSiHpgkc13afbAm18TzLB_OU1ctwDAcfKgtd3jgFeoD4fEaoZeStfw083wJUjnMI6FsSzY2fZ3DrmstF9HFqmtkEl0qfFYbjzagCNMN3QiJT4toQyWnfoapEiuvA-e9RO97hbZ6G5hkXc0Vp-gv5pgovByseBW2RlX4uwcpJEHUmyoI4X6FF-yVswDsufWob7l4WvV1Bvt_SMVi11bqn7L0E0au";
+var bearer = "BQCQsoRlQX8sLaAwTfmd0RuBU5eXZvQBV3_DlXqTvW94X0EM55amIkBPXH_caep5aJFVOEmL_TJZhfHtgvwjKHq6byoQe69Wv1vQuWdarAtEnU2KpSfztlUa89iSKmi8melF4Ws-YbBo2FBQp5IOfycU0JB-_kEA9_Q6OKb9qNcqvzyoAv4kIGw8TxoNq5yYeH9tZGCh2AmK4f2c0fv497vnwni3eHx8uD-QBj_s6djpOQRuoAW3RyhoAFjlKfwVaCQzy0Yi5niUsU_GzTYq_lCpNraMetSUUxfqdRNQTcLZ";
 
+/*
+* Temporary backdoor method of updating OAuth key
+* Login to your Spotify Developer account and generate new OAuth token
+* Copy to clipboard and go to UniSound
+* Access from console and call updateOAuth() with no parameters
+* Paste into prompt and run without reloading (which will reset token)
+*/
+function updateOAuth(){
+	var newToken = prompt("Paste OAuth Token Here:");
+	alert("...with OAuth(" + cutKey(bearer, 5) + ")");
+	bearer = newToken;
+	alert("...with OAuth(" + cutKey(bearer, 5) + ")");
+	alert("Complete. Do not reload the page, or the OAuth token will revert.");
+}
+
+function cutKey(key, cut){
+	if(key.length >= cut * 2){
+		return key.substr(0, cut) + "..." + key.substr(key.length - cut, cut);	
+	}
+	else{
+		return key;
+	}
+}
 
 function serveData(data){
 	var incomingData = document.getElementById('incomingData');
