8788630edc130182875b61a82ed42cfcf558056a
mattprice09 <mattprice09@rocketmail.com>
2015-10-07 17:03:00 -0500

    test

--- database.js
+++ database.js
@@ -10,41 +10,43 @@ var initData = function() {
 		console.log(json);
 		console.log("Type of 'json': " + typeof json);
 
-		for (var i = 0; i < json["SDpairs"].length; i++) {
-			console.log(json["SDPairs"][i]);
-			console.log("Type of 'edge': " + typeof json["SDpairs"][i]);
-			if (currPair == null) {
-				// If it is the first JSON to be read
-				currPair = [];
-				currPair.push(json["SDPairs"][i]);
-			} else if (json["SDPairs"][i]['SD Pair'] == currPair[0]['SD Pair']) {
-				// If the JSON is part of the same SD pair as currPair, push into currPair
-				currPair.push(json["SDPairs"][i]);
-			} else {
-				// If it is a new SDpair as compared to currPair
-				sdPairs.push(currPair);
-				currPair = [];
-				currPair.push(json["SDPairs"][i]);
-			}
-		}
-		
-		// for (var edge in json["SDpairs"]) {
-		// 	console.log(json["SDPairs"][edge]);
-		// 	console.log("Type of 'edge': " + typeof edge);
+		// for (var i = 0; i < json["SDpairs"].length; i++) {
+		// 	console.log(json["SDPairs"][i]);
+		// 	console.log("Type of 'edge': " + typeof json["SDpairs"][i]);
 		// 	if (currPair == null) {
 		// 		// If it is the first JSON to be read
 		// 		currPair = [];
-		// 		currPair.push(edge);
-		// 	} else if (edge['SD Pair'] == currPair[0]['SD Pair']) {
+		// 		currPair.push(json["SDPairs"][i]);
+		// 	} else if (json["SDPairs"][i]['SD Pair'] == currPair[0]['SD Pair']) {
 		// 		// If the JSON is part of the same SD pair as currPair, push into currPair
-		// 		currPair.push(edge);
+		// 		currPair.push(json["SDPairs"][i]);
 		// 	} else {
 		// 		// If it is a new SDpair as compared to currPair
 		// 		sdPairs.push(currPair);
 		// 		currPair = [];
-		// 		currPair.push(edge);
+		// 		currPair.push(json["SDPairs"][i]);
 		// 	}
 		// }
+
+		for (var name in json["SDpairs"]) {
+			var edge = json["SDpairs"][name];
+			
+			console.log(json["SDPairs"][edge]);
+			console.log("Type of 'edge': " + typeof edge);
+			if (currPair == null) {
+				// If it is the first JSON to be read
+				currPair = [];
+				currPair.push(edge);
+			} else if (edge['SD Pair'] == currPair[0]['SD Pair']) {
+				// If the JSON is part of the same SD pair as currPair, push into currPair
+				currPair.push(edge);
+			} else {
+				// If it is a new SDpair as compared to currPair
+				sdPairs.push(currPair);
+				currPair = [];
+				currPair.push(edge);
+			}
+		}
 
 		// $.each($.parseJSON(data["SDPairs"]), function(doc) {
 		// 	console.log(doc);
