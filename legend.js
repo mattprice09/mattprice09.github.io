98d2e34f71426f6990fbff32cddb3e84a0b96abb
mattprice09 <mattprice09@rocketmail.com>
2015-10-07 13:11:09 -0500

    testing

--- legend.js
+++ legend.js
@@ -1,5 +1,9 @@
 var loadLegend = function() {
+	var pair;
+	var num = 0;
 	for (sdPair in sdPairs) {
+		pair = document.createElement("div");
+		pair.setAttribute("id", sdPairs[num]["SD Pair"]);
 		
 	}
 }
