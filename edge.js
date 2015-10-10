e5e19c88f31415a2a61152045288a807658b6819
mattprice09 <mattprice09@rocketmail.com>
2015-10-10 00:54:02 -0500

    Started edge and node classes

---
+++ edge.js
@@ -0,0 +1,37 @@
+var source = "";
+var dest = "";
+var flow = "";
+var capacity = "";
+
+// ~~~~~Getters and setters below here~~~~~
+function getSource() {
+	return this.source;
+}
+
+function setSource(country) {
+	this.source = country;
+}
+
+function getDest() {
+	return this.dest;
+}
+
+function setDest(country) {
+	this.dest = country;
+}
+
+function getFlow() {
+	return this.flow;
+}
+
+function setFlow(num) {
+	this.flow = num;
+}
+
+function getCapacity() {
+	return this.capacity;
+}
+
+function setCapacity(num) {
+	this.capacity = num;
+}
---
+++ node.js
@@ -0,0 +1,12 @@
+var name = "";
+var inDegree = "";
+var outDegree = "";
+
+// ~~~~~Getters and setters below here~~~~~
+function getName() {
+	return this.name;
+}
+
+function setName(str) {
+	this.name = str;
+}
