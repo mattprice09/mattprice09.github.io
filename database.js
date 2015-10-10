710ba3eeae240beb693980ae5289eefdeb1af85b
mattprice09 <mattprice09@rocketmail.com>
2015-10-09 22:52:28 -0500

    test

--- database.js
+++ database.js
@@ -1,30 +1,55 @@
 var sdPairs = [];
-var currPair = [];
-var db = this;
 
-var initData() = function() {
+
+function initData() {
 	// Create and push arrays of JSONs into sdPairs. 
 	// Each array contains edges from the same SD Pair
 	$.getJSON("SDpairs.json", function(data) {
 		
-		for (var element in data["SDpairs"]) {
-			var edge = data["SDpairs"][element];
+		initalize(data);
 
-			if (db.currPair.length == 0) {
-				// If it is the first JSON to be read
-				db.currPair.push(edge);
-			} else if (edge['SD Pair'] == db.currPair[0]['SD Pair']) {
-				// If the JSON is part of the same SD pair as currPair, push into currPair
-				db.currPair.push(edge);
-			} else {
-				// If it is a new SDpair as compared to currPair
-				db.sdPairs.push(db.currPair);
-				db.currPair = [];
-				db.currPair.push(edge);
-			}
-		}
-		db.sdPairs.push(db.currPair);
+		// for (var element in data["SDpairs"]) {
+		// 	var edge = data["SDpairs"][element];
+
+		// 	if (currPair.length == 0) {
+		// 		// If it is the first JSON to be read
+		// 		currPair.push(edge);
+		// 	} else if (edge['SD Pair'] == currPair[0]['SD Pair']) {
+		// 		// If the JSON is part of the same SD pair as currPair, push into currPair
+		// 		currPair.push(edge);
+		// 	} else {
+		// 		// If it is a new SDpair as compared to currPair
+		// 		sdPairs.push(currPair);
+		// 		currPair = [];
+		// 		currPair.push(edge);
+		// 	}
+		// }
+		// sdPairs.push(currPair);
 	});
-	console.log(db.sdPairs.length);
-	console.log(db.currPair.length);
+	// console.log(sdPairs.length);
+	// console.log(currPair.length);
+}
+
+function initialize(data) {
+	var currPair = [];
+
+	for (var element in data["SDpairs"]) {
+		var edge = data["SDpairs"][element];
+
+		if (currPair.length == 0) {
+			// If it is the first JSON to be read
+			currPair.push(edge);
+		} else if (edge['SD Pair'] == currPair[0]['SD Pair']) {
+			// If the JSON is part of the same SD pair as currPair, push into currPair
+			currPair.push(edge);
+		} else {
+			// If it is a new SDpair as compared to currPair
+			sdPairs.push(currPair);
+			currPair = [];
+			currPair.push(edge);
+		}
+	}
+	sdPairs.push(currPair);
+	console.log(sdPairs.length);
+	console.log(currPair.length);
 }
