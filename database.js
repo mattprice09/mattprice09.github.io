816eb8ce98ea2d6bf50f2cc6b51a8bd437325dd4
mattprice09 <mattprice09@rocketmail.com>
2015-10-09 22:23:55 -0500

    Testing Callback

--- database.js
+++ database.js
@@ -1,43 +1,37 @@
 var sdPairs = [];
 
-var initData = function() {
+function initData() {
 	var currPair = [];
-
+	var db = this;
 	// Create and push arrays of JSONs into sdPairs. 
 	// Each array contains edges from the same SD Pair
 	$.getJSON("SDpairs.json", function(data) {
 		
-		//var str = JSON.stringify(data);
-		//var json = JSON.parse(str);
 		var num = 0;
 		for (var element in data["SDpairs"]) {
 			var edge = data["SDpairs"][element];
 
-			if (currPair.length == 0) {
+			if (db.currPair.length == 0) {
 				// If it is the first JSON to be read
-				currPair.push(edge);
-			} else if (edge['SD Pair'] == currPair[0]['SD Pair']) {
+				db.currPair.push(edge);
+			} else if (edge['SD Pair'] == db.currPair[0]['SD Pair']) {
 				// If the JSON is part of the same SD pair as currPair, push into currPair
-				currPair.push(edge);
+				db.currPair.push(edge);
 			} else {
 				// If it is a new SDpair as compared to currPair
-				sdPairs.push(currPair);
-				currPair = [];
-				currPair.push(edge);
+				db.sdPairs.push(db.currPair);
+				db.currPair = [];
+				db.currPair.push(edge);
 			}
-			
+
 			if (num == 0) {
 				console.log("Edge is " + edge);
 				console.log("edge['SD Pair'] is " + edge['SD Pair']);
 			}
 			num++;
 		}
-		sdPairs.push(currPair);
+		db.sdPairs.push(db.currPair);
 	});
-
 	console.log(sdPairs.length);
 	console.log(currPair.length);
-
-	
-	
 }
