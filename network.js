637b467a39e91ef81cb70151b536bee1342ba54d
mattprice09 <mattprice09@rocketmail.com>
2015-10-10 11:26:19 -0500

    Call

--- network.js
+++ network.js
@@ -5,7 +5,7 @@ var isActive;
 
 // Network constructor
 var network = function(arr) {
-	network.prototype.createNetwork();
+	network.prototype.createNetwork().call(this);
 	var src;
 	var dst;
 	var flw;
