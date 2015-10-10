d9987e76a4a16dc39c1a04795dd5978d9198cf40
mattprice09 <mattprice09@rocketmail.com>
2015-10-09 23:05:52 -0500

    Implemented loadLegend()

--- database.js
+++ database.js
@@ -1,35 +1,15 @@
 var sdPairs = [];
 
-
+// Create and push arrays of JSONs into sdPairs. 
+// Each array contains edges from the same SD Pair
 function initData() {
-	// Create and push arrays of JSONs into sdPairs. 
-	// Each array contains edges from the same SD Pair
-	$.getJSON("SDpairs.json", function(data) {
-		
+
+	$.getJSON("SDpairs.json", function(data) {		
 		initialize(data);
-
-		// for (var element in data["SDpairs"]) {
-		// 	var edge = data["SDpairs"][element];
-
-		// 	if (currPair.length == 0) {
-		// 		// If it is the first JSON to be read
-		// 		currPair.push(edge);
-		// 	} else if (edge['SD Pair'] == currPair[0]['SD Pair']) {
-		// 		// If the JSON is part of the same SD pair as currPair, push into currPair
-		// 		currPair.push(edge);
-		// 	} else {
-		// 		// If it is a new SDpair as compared to currPair
-		// 		sdPairs.push(currPair);
-		// 		currPair = [];
-		// 		currPair.push(edge);
-		// 	}
-		// }
-		// sdPairs.push(currPair);
 	});
-	// console.log(sdPairs.length);
-	// console.log(currPair.length);
 }
 
+// Callback function for initData()
 function initialize(data) {
 	var currPair = [];
 
@@ -52,6 +32,9 @@ function initialize(data) {
 			num++;
 		}
 	}
+
+	loadLegend();
+	
 	sdPairs.push(currPair);
 	console.log("Expected no. of SD pairs: " + num);
 	console.log(sdPairs.length);
